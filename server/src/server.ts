1fe335270bd537a08d5a22f66acba155feb6116d
ViniMonteiro1990 <vinimonteirolg50@gmail.com>
2020-06-02 17:51:41 -0300

    master

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,15 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes';
+import cors from 'cors';
 
 const app = express()
 
-app.get('/users', (req,res) => {
-    console.log('Listagem de usuarios')
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
-    res.json([
-        'Vinicius',
-        'Monica',
-        'Marilda',
-        'Julio',
-        'Gui',
-        'Dana'
-    ])
-})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
 
 app.listen(3333);
